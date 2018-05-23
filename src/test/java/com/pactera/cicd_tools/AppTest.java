a958bcb8be10e4d6fdeafecb771340eff761d4c2
LilaPeng <2860408731@qq.com>
2018-05-23 14:42:03 +0800

    Commit maven project helloword.

---
+++ src/test/java/com/pactera/cicd_tools/AppTest.java
@@ -0,0 +1,20 @@
+package com.pactera.cicd_tools;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
