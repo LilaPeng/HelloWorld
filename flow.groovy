694f3f42cf8617734ea5f21efd5f587c3f65f8b4
LilaPeng <39294204+LilaPeng@users.noreply.github.com>
2018-06-01 16:13:29 +0800

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -1,35 +1,38 @@
-stage 'build'
-node {
+stage('build'){
+    node {
      git 'https://gitee.com/LilaPeng/HelloWorld.git'
-     withEnv(["PATH+MAVEN=${tool 'mvn'}/bin"]) {
-          sh "mvn -B –Dmaven.test.failure.ignore=true clean package"
-     }
+     //withEnv(["PATH+MAVEN=${tool 'mvn'}/bin"]) {
+          //sh "mvn -B –Dmaven.test.failure.ignore=true clean package"
+          //sh "mvn clean package"
+     //}
+     def mvnHome = tool 'mvn'
+     sh "${mvnHome}/bin/mvn -B -Dmaven.test.failure.ignore verify"
      stash excludes: 'target/', includes: '**', name: 'source'
+    }
 }
-stage 'test'
-parallel 'integration': { 
+
+stage('test'){
+    parallel 'integration': { 
      node {
           unstash 'source'
           withEnv(["PATH+MAVEN=${tool 'mvn'}/bin"]) {
                sh "mvn clean verify"
           }
      }
-}, 'quality': {
+    }, 'quality': {
      node {
           unstash 'source'
-          withEnv(["PATH+MAVEN=${tool 'mvn'}/bin"]) {
-               sh "mvn sonar:sonar"
           }
      }
 }
-stage 'approve'
-timeout(time: 7, unit: 'DAYS') {
-     input message: 'Do you want to deploy?', submitter: 'ops'
-}
-stage name:'deploy', concurrency: 1
-node {
+
+stage('approve'){
+    timeout(time: 7, unit: 'DAYS') {    
+    input 'Ready to go?'
+    // rest as before
+    }
+    node {
      unstash 'source'
-     withEnv(["PATH+MAVEN=${tool 'mvn'}/bin"]) {
-          sh "mvn cargo:deploy"
-     }
+         }
 }
+
